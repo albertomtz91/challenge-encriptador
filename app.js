fec3e091619d98584908e2f1dcdee9cdf1aa439a
Alberto Martinez <artack_darcknes666@hotmail.com>
2024-02-29 16:18:05 -0600

    primera parte encriptador

---
+++ app.js
@@ -0,0 +1,57 @@
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+
+//La letra "e" es convertida para "enter"
+//La letra "i" es convertida para "imes"
+//La letra "a" es convertida para "ai"
+//La letra "o" es convertida para "ober"
+//La letra "u" es convertida para "ufat"
+
+function btnEncriptar(){
+    const textoEncriptado = encriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none"
+}
+
+function encriptar(stringEncriptada){
+
+    let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+
+    stringEncriptada = stringEncriptada.toLowerCase()
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+
+        if(stringEncriptada.includes(matrizCodigo[i][0])){
+
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+
+        }
+    }
+    return stringEncriptada
+}
+
+function btnDesencriptar(){
+    const textoEncriptado = desencriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = "";
+    
+}
+
+function desencriptar(stringDEsncriptada){
+
+    let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+
+    stringDEsncriptada = stringDEsncriptada.toLowerCase()
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+
+        if(stringDEsncriptada.includes(matrizCodigo[i][1])){
+
+            stringDEsncriptada = stringDEsncriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+
+        }
+    }
+    return stringDEsncriptada
+}
+
